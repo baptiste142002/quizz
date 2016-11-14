6a2644d67c721bf593c7eccbd8bfbade6225e0f3
BONJEAN-MORET Baptiste <BONJEAN-MORET Baptiste>
2016-11-14 16:16:45 +0100

    premiere vrsion du quizz

---
+++ brouillon.py
@@ -0,0 +1,7 @@
+for i in [1,2,3]:
+    print(i)
+    print (i + 10)
+print (4)
+
+
+
--- quizz.py
+++ quizz.py
@@ -4,3 +4,20 @@ traductions = {
     "car" : "voiture"
 }
 
+
+print ("Hello, welcome to the quizz")
+
+for question in traductions:
+    user_answer = input (question + " est : " )
+    real_answer = traductions [question]
+
+    if user_answer == real_answer :
+        print ("correct")
+    else :
+        print ("faux")
+
+
+
+
+
+
